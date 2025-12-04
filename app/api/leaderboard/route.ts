9cd80ae6ae4c069793c10b3be8579d72ab5e1677
thursdaycapital <lizesheng223@gmail.com>
2025-12-04 21:58:44 +0800

    Update route.ts

--- app/api/leaderboard/route.ts
+++ app/api/leaderboard/route.ts
@@ -1,15 +1,20 @@
-import kv from "import kv from "../../../lib/kv";
+import kv from "../../../lib/kv";
 import { NextResponse } from "next/server";
+
 export async function GET() {
   const today = new Date().toISOString().split("T")[0];
   const prefix = `tap:*:${today}`;
   const keys = await kv.keys(prefix);
-  const data = [];
+
+  const data: { fid: string; count: number }[] = [];
+
   for (const key of keys) {
     const count = await kv.get<number>(key);
     const fid = key.split(":")[1];
     data.push({ fid, count: count || 0 });
   }
+
   data.sort((a, b) => b.count - a.count);
+
   return NextResponse.json(data.slice(0, 20));
 }
